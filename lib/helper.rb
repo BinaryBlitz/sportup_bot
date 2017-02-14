99409d389ae233583705d596f5408ca7e6100e8f
SumLare <mrhard95@gmail.com>
2017-02-14 21:58:25 +0300

    Add buttons helper

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,4 +1,5 @@
 require_relative 'helper/date'
+require_relative 'helper/buttons'
 require_relative 'helper/validators'
 require_relative 'helper/teams'
 require_relative 'helper/vote'
---
+++ lib/helper/buttons.rb
@@ -0,0 +1,25 @@
+require 'telegram/bot'
+
+module Helper
+  module Buttons
+    AVAILABLE_LANGS = {
+      en: 'English',
+      ru: 'Русский'
+    }
+
+    def keyboard_buttons(button_list)
+      Telegram::Bot::Types::ReplyKeyboardMarkup.new(
+        keyboard: button_list,
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        selective: true
+      )
+    end
+
+    def language_list
+      AVAILABLE_LANGS.values.map do |lang|
+        [] << { text: lang }
+      end
+    end
+  end
+end
