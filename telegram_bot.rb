3988cdec3172af69dd26d93a021112fcc411198b
SumLare <mrhard95@gmail.com>
2017-04-08 22:17:49 +0300

    Install tilt, render active events

--- telegram_bot.rb
+++ telegram_bot.rb
@@ -2,9 +2,21 @@ require_relative 'lib/bot_message_dispatcher'
 require_relative 'lib/models/user'
 require './environment'
 require 'json'
+require 'tilt'
 
 class TelegramBot
   def call(env)
+    req = Rack::Request.new(env)
+    if req.path_info == '/events'
+      return [200, { 'Content-Type' => 'text/html' }, [template]]
+    else
+      return empty_response if env['rack.input'].read.empty?
+      env['rack.input'].rewind
+      webhook_message(env)
+    end
+  end
+
+  def webhook_message(env)
     @webhook_message = JSON.parse(env['rack.input'].read)
     BotMessageDispatcher.new(@webhook_message, user).process
     empty_response
@@ -14,12 +26,16 @@ class TelegramBot
     ['200', { 'Content-Type' => 'application/json' }, []]
   end
 
+  def template
+    template = Tilt.new('views/events.html.erb').render
+  end
+
   def from
     return if @webhook_message['edited_channel_post'] || @webhook_message.dig('channel_post', 'from')
     if @webhook_message['callback_query'].nil?
       @webhook_message.dig('message', 'from') || @webhook_message.dig('edited_message', 'from')
     else
-      @webhook_message['callback_query']['from']
+      @webhook_message.dig('callback_query', 'from')
     end
   end
 
