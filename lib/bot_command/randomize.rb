d630ec3dab4b6485f1b5f2e0ab61f795be414219
SumLare <mrhard95@gmail.com>
2017-04-29 00:01:18 +0300

    Randomize by event's team limit

--- lib/bot_command/randomize.rb
+++ lib/bot_command/randomize.rb
@@ -3,28 +3,12 @@ module BotCommand
     include Helper::Validators
 
     def should_start?
-      return false if text.nil?
-      text.start_with?('/randomize') || text.start_with?("/randomize@#{bot_name}")
+      text == '/randomize' || text == "/randomize@#{bot_name}"
     end
 
     def start
-      if command_without_params?(text, '/randomize')
-        send_message_with_reply(I18n.t('number_of_teams'))
-        user.next_bot_command(method: :number, class: self.class.to_s)
-      else
-        number = text.gsub(/\/randomize\s+/, '').to_i
-        valid_number_of_teams?(number, event) do |number|
-          send_message("#{I18n.t('teams_list')}: \n#{event.random_teams_list(number)}")
-          user.reset_next_bot_command
-        end
-      end
-    end
-
-    def number
-      valid_number_of_teams?(text, event) do |number|
-        send_message("#{I18n.t('teams_list')}: \n#{event.random_teams_list(number.to_i)}")
-        user.reset_next_bot_command
-      end
+      send_message("#{I18n.t('teams_list')}: \n#{event.random_teams_list(event.team_limit)}")
+      user.reset_next_bot_command
     end
   end
 end
