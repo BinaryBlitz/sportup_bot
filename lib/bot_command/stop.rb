903ed5cb57c17b1f39ac252666e091be2835f798
SumLare <mrhard95@gmail.com>
2017-05-24 22:55:07 +0300

    Destroy event in app after stop command

--- lib/bot_command/stop.rb
+++ lib/bot_command/stop.rb
@@ -6,6 +6,7 @@ module BotCommand
 
     def start
       send_message(I18n.t('event_cancellation'))
+      AppEvent.find_by(chat_id: event.chat.chat_id)&.destroy
       event&.destroy
       user.reset_next_bot_command
     end
--- lib/models/app_event.rb
+++ lib/models/app_event.rb
@@ -2,4 +2,6 @@ require 'active_record'
 
 class AppEvent < App
   self.table_name = 'events'
+
+  has_many :memberships, class_name: 'AppMembership', foreign_key: 'event_id', dependent: :destroy
 end
